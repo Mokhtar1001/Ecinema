6b23710c9ab133dd2bd982e18bd970b443a20d1d
Mokhtar1001 <mokhtaryehia1001@gmail.com>
2025-10-19 23:59:42 +0300

    first commit

---
+++ ECinema/ViewModels/MovieVM.cs
@@ -0,0 +1,10 @@
+﻿using ECinema.Models;
+
+namespace ECinema.ViewModels
+{
+    internal class MovieVM
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Cinema> Cinemas { get; set; }
+    }
+}
