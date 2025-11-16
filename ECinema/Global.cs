bbbc3e648bdadfad6557bddf11e981f46c92581d
Mokhtar1001 <mokhtaryehia1001@gmail.com>
2025-11-16 14:17:24 +0200

    Last MVC

--- ECinema/Global.cs
+++
@@ -1,6 +0,0 @@
-﻿
-global using ECinema.DataAccess;
-global using ECinema.ViewModels;
-global using ECinema.Models;
-global using ECinema.Repositories;
-global using ECinema.Repositories.IRepositories;
--- ECinema/obj/Debug/net9.0/ECinema.AssemblyInfo.cs
+++ ECinema/obj/Debug/net9.0/ECinema.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9909193b2844b54b6c41e8bfadcb8639642b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb07e0149b536531e79bba14fe19981373b9a240")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
